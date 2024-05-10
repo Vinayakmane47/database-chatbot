a1297b0dff9327c14e1ee78b64ccea74e2f40b8c
Vinayak Mane <manevinayakv.47@gmail.com>
2024-05-11 03:34:23 +1000

    final commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from dotenv import load_dotenv 
 from langchain_community.utilities.sql_database import SQLDatabase
 import streamlit as st 
+from langchain_core.messages import AIMessage , HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
 
 load_dotenv()
@@ -9,6 +14,80 @@ def init_database(user:str,password:str,host:str,port:str,database:str):
     db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)
 
+def get_sql_chain(db): 
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+    
+    <SCHEMA>{schema}</SCHEMA>
+    
+    Conversation History: {chat_history}
+    
+    
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatOpenAI()
+
+    def get_schema(_): 
+        return db.get_table_info()
+
+    return (
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt 
+        | llm
+        | StrOutputParser()
+
+    )
+
+def get_response(user_query:str , db: SQLDatabase , chat_history:list): 
+    sql_chain = get_sql_chain(db)
+
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, question, sql query, and sql response, write a natural language response.
+    <SCHEMA>{schema}</SCHEMA>
+
+    Conversation History: {chat_history}
+    SQL Query: <SQL>{query}</SQL>
+    User question: {question}
+    SQL Response: {response}"""
+
+    prompt = ChatPromptTemplate.from_template(template) 
+
+    llm = ChatOpenAI()
+
+    chain = (
+        RunnablePassthrough.assign(query=sql_chain).assign(
+            schema=lambda _:db.get_table_info(),
+            response = lambda vars : db.run(vars["query"]),
+        )
+        | prompt 
+        | llm 
+        | StrOutputParser()
+    )
+
+    return chain.invoke({
+        "question" : user_query , 
+        "chat_history" : chat_history
+    })
+
+if "chat_history" not in st.session_state: 
+    st.session_state.chat_history = [
+        AIMessage("Hello! I'm a SQL assistant . Ask me anything about your database."),
+        HumanMessage("hi")
+    ]
 
 st.set_page_config(page_title="Chat with MySQL",page_icon="speech_balloon")
 
@@ -38,8 +117,29 @@ with st.sidebar:
             st.success("Connected to database!")
 
 
+for message in st.session_state.chat_history: 
+    if isinstance(message,AIMessage): 
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message,HumanMessage): 
+        with st.chat_message("Human"): 
+            st.markdown(message.content)
 
+user_query = st.chat_input("Enter your message.....")
 
-st.chat_input("Enter your message.....")
+if user_query is not None and user_query.strip !="": 
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"): 
+        st.markdown(user_query)
+
+    with st.chat_message("AI"): 
+        sql_chain = get_sql_chain(st.session_state.db)
+        response = get_response(user_query,st.session_state.db , st.session_state.chat_history)
+        
+        #response = "I dont know "
+        st.markdown(response)
+
+    st.session_state.chat_history.append(AIMessage(content=response))
 
 
